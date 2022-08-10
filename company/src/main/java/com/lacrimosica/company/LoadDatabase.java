f8a083882b26617351368d2314390a8a7c57b0b1
Mohammad Goudarzi <m.goudarzi77@hotmail.com>
2022-08-09 17:53:57 -0700

    keycloack
    just added dependencies to pom.xml

--- company/src/main/java/com/lacrimosica/company/LoadDatabase.java
+++ company/src/main/java/com/lacrimosica/company/LoadDatabase.java
@@ -1,38 +1,38 @@
-package com.lacrimosica.company;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-class LoadDatabase {
-
-    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
-
-    @Bean
-    CommandLineRunner initDatabase(EmployeeRepository employeeRepository,
-                                   OrderRepository orderRepository,
-                                   ProjectRepository projectRepository) {
-
-        return args -> {
-            employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar"));
-            employeeRepository.save(new Employee("Frodo", "Baggins", "thief"));
-
-            employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
-
-
-            orderRepository.save(new Order("MacBook Pro", OrderStatus.COMPLETED));
-            orderRepository.save(new Order("iPhone", OrderStatus.IN_PROGRESS));
-
-            orderRepository.findAll().forEach(order -> {
-                log.info("Preloaded " + order);
-            });
-
-            projectRepository.save(new Project("Project 1" ));
-            projectRepository.save(new Project("Project 2" ));
-            projectRepository.save(new Project("Project 3" ));
-        };
-    }
-}
+//package com.lacrimosica.company;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//class LoadDatabase {
+//
+//    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+//
+//    @Bean
+//    CommandLineRunner initDatabase(EmployeeRepository employeeRepository,
+//                                   OrderRepository orderRepository,
+//                                   ProjectRepository projectRepository) {
+//
+//        return args -> {
+//            employeeRepository.save(new Employee("Bilbo", "Baggins", "burglar"));
+//            employeeRepository.save(new Employee("Frodo", "Baggins", "thief"));
+//
+//            employeeRepository.findAll().forEach(employee -> log.info("Preloaded " + employee));
+//
+//
+//            orderRepository.save(new Order("MacBook Pro", OrderStatus.COMPLETED));
+//            orderRepository.save(new Order("iPhone", OrderStatus.IN_PROGRESS));
+//
+//            orderRepository.findAll().forEach(order -> {
+//                log.info("Preloaded " + order);
+//            });
+//
+//            projectRepository.save(new Project("Project 1" ));
+//            projectRepository.save(new Project("Project 2" ));
+//            projectRepository.save(new Project("Project 3" ));
+//        };
+//    }
+//}
