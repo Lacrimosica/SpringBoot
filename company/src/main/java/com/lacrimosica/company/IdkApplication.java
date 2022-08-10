38c3f44c4071c3b36e34d26fa66b2b29140e9773
Mohammad Goudarzi <m.goudarzi77@hotmail.com>
2022-08-10 20:36:35 +0200

    idk

--- company/src/main/java/com/lacrimosica/company/IdkApplication.java
+++ company/src/main/java/com/lacrimosica/company/IdkApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CompanyApplication {
+public class IdkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CompanyApplication.class, args);
+		SpringApplication.run(IdkApplication.class, args);
 	}
 
 }
